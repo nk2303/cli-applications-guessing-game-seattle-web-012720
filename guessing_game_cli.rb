804607b7d120796098531d38489aea588f4f23b1
Ngan Kim Khong <ngankhong03@gmail.com>
2020-01-20 06:41:23 +0000

    Automatically backed up by Learn

--- guessing_game_cli.rb
+++ guessing_game_cli.rb
@@ -1,7 +1,7 @@
 # Code your solution here!
 def run_guessing_game
   num = rand(6) + 1
-  guess = gets.chomp
+  guess = gets.chomp.to_i
   if num == guess
     print "You guessed the correct number!"
   elsif guess == "exit"
